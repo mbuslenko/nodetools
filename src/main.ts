a867dbf8129fdb11dc1c9a04a04ef139888c8cd4
Mike <49212643+mbuslenko@users.noreply.github.com>
2022-07-05 15:34:55 +0300

    🖥 macos ask for permission

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,18 @@ import {
   Menu,
   nativeImage,
   Tray,
-} from "electron";
-import * as path from "path";
-import * as domains from "./domains";
-import { changeSettings, initSettings } from "./settings";
-import settings from "./settings";
-import { ShortcutsSettings } from "./settings/settings.types";
+} from 'electron';
+import * as path from 'path';
+import * as domains from './domains';
+import { changeSettings, initSettings } from './settings';
+import settings from './settings';
+import { ShortcutsSettings } from './settings/settings.types';
 import { openWebURL } from './shared/utils/open-website';
+import { askForAccessibilityAccess, getAuthStatus } from 'node-mac-permissions';
+import ErrorsHandler from './errors/errors.module';
 
-require("update-electron-app")({
-  repo: "mbuslenko/nodetools",
+require('update-electron-app')({
+  repo: 'mbuslenko/nodetools',
 });
 
 function createWindow(pathToHtmlFile: string) {
@@ -25,10 +27,10 @@ function createWindow(pathToHtmlFile: string) {
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
-      preload: path.join(__dirname, "preload.js"),
+      preload: path.join(__dirname, 'preload.js'),
     },
     width: 800,
-    icon: "../src/assets/app-icon.png",
+    icon: '../src/assets/app-icon.png',
   });
 
   // and load the index.html of the app.
@@ -37,165 +39,188 @@ function createWindow(pathToHtmlFile: string) {
   app.dock.show();
 }
 
-ipcMain.on("change-settings", (event, arg) => {
-  console.log("Settings was changed", JSON.stringify(arg));
+ipcMain.on('change-settings', (event, arg) => {
+  console.log('Settings was changed', JSON.stringify(arg));
 
   changeSettings(arg.data);
 });
 
-ipcMain.handle("get-errors", (event, arg) => {
-  return settings.get("errorsStorage");
+ipcMain.handle('get-errors', (event, arg) => {
+  return settings.get('errorsStorage');
 });
 
 app.whenReady().then(() => {
   initSettings();
-  const shortcuts = settings.get("shortcuts") as ShortcutsSettings;
+  const shortcuts = settings.get('shortcuts') as ShortcutsSettings;
   const InlineDomain = new domains.inline.InlineDomain();
 
+  if (getAuthStatus('accessibility') === 'denied') {
+    const errorsHandler = new ErrorsHandler();
+
+    //@ts-ignore
+    askForAccessibilityAccess().then((status: string) => {
+      if (status === 'denied') {
+        errorsHandler.handleError({
+          message: 'Accessibility access is denied',
+          environment: 'Nodetools',
+          trace: null,
+        });
+      }
+    });
+
+    if (getAuthStatus('accessibility') === 'denied') {
+      errorsHandler.handleError({
+        message: 'You have to grant Accessibility permission for Nodetools to work it correctly, open Settings -> Security & Privacy -> Privacy tab -> Accessibility -> Add Nodetools',
+        environment: 'Nodetools',
+        trace: null,
+      });
+    }
+  }
+
   // * translate shortcut
-  globalShortcut.register(shortcuts.translate.join("+"), async () => {
+  globalShortcut.register(shortcuts.translate.join('+'), async () => {
     await InlineDomain.translateText();
   });
 
   // * transliterator shortcut
-  globalShortcut.register(shortcuts.transliterate.join("+"), async () => {
+  globalShortcut.register(shortcuts.transliterate.join('+'), async () => {
     await InlineDomain.transliterateText();
   });
 
   // * currency convertor shortcut
-  globalShortcut.register(shortcuts.convertCurrency.join("+"), async () => {
+  globalShortcut.register(shortcuts.convertCurrency.join('+'), async () => {
     await InlineDomain.convertCurrency();
   });
 
   // * humanize shortcut
-  globalShortcut.register(shortcuts.humanizeText.join("+"), async () => {
+  globalShortcut.register(shortcuts.humanizeText.join('+'), async () => {
     await InlineDomain.humanizeText();
   });
 
   // * spell checker shortcut
-  globalShortcut.register(shortcuts.spellCheck.join("+"), async () => {
+  globalShortcut.register(shortcuts.spellCheck.join('+'), async () => {
     await InlineDomain.spellCheck();
   });
 
   // * url shortener shortcut
-  globalShortcut.register(shortcuts.shortenUrl.join("+"), async () => {
+  globalShortcut.register(shortcuts.shortenUrl.join('+'), async () => {
     await InlineDomain.shortenUrl();
   });
 
   // * calculate shortcut
-  globalShortcut.register(shortcuts.calculate.join("+"), async () => {
+  globalShortcut.register(shortcuts.calculate.join('+'), async () => {
     await InlineDomain.calculate();
   });
 });
 
 let tray: Tray;
 app.whenReady().then(() => {
-  if (process.platform === "darwin") {
+  if (process.platform === 'darwin') {
     app.dock.hide();
   }
 
   const icon = nativeImage.createFromPath(
-    path.join(__dirname, "../src/assets/tray-icon.png")
+    path.join(__dirname, '../src/assets/tray-icon.png')
   );
   tray = new Tray(icon);
 
-  const shortcuts = settings.get("shortcuts") as ShortcutsSettings;
+  const shortcuts = settings.get('shortcuts') as ShortcutsSettings;
   const InlineDomain = new domains.inline.InlineDomain();
 
   const contextMenu = Menu.buildFromTemplate([
     {
-      label: "About",
-      role: "about",
+      label: 'About',
+      role: 'about',
       click: () => openWebURL('https://nodetools.app/about'),
     },
     {
-      label: "Preferences",
+      label: 'Preferences',
       submenu: [
         {
-          label: "Shortcuts",
-          role: "window",
-          click: () => createWindow("../index.html"),
+          label: 'Shortcuts',
+          role: 'window',
+          click: () => createWindow('../index.html'),
         },
         {
-          label: "Translate options",
-          role: "window",
-          click: () => createWindow("../index.html"),
+          label: 'Translate options',
+          role: 'window',
+          click: () => createWindow('../index.html'),
         },
         {
-          label: "Currency converter options",
-          role: "window",
-          click: () => createWindow("../index.html"),
+          label: 'Currency converter options',
+          role: 'window',
+          click: () => createWindow('../index.html'),
         },
         {
-          label: "Errors",
-          role: "window",
-          click: () => createWindow("../errors-list.html"),
+          label: 'Errors',
+          role: 'window',
+          click: () => createWindow('../errors-list.html'),
         },
       ],
     },
-    { label: "Separator", type: "separator" },
+    { label: 'Separator', type: 'separator' },
     {
-      label: "Translate",
-      accelerator: shortcuts.translate.join("+"),
-      role: "help",
+      label: 'Translate',
+      accelerator: shortcuts.translate.join('+'),
+      role: 'help',
       click: async () => {
         await InlineDomain.translateText();
       },
     },
     {
-      label: "Transliterate",
-      accelerator: shortcuts.transliterate.join("+"),
-      role: "help",
+      label: 'Transliterate',
+      accelerator: shortcuts.transliterate.join('+'),
+      role: 'help',
       click: async () => {
         await InlineDomain.transliterateText();
       },
     },
     {
-      label: "Humanize",
-      accelerator: shortcuts.humanizeText.join("+"),
-      role: "help",
+      label: 'Humanize',
+      accelerator: shortcuts.humanizeText.join('+'),
+      role: 'help',
       click: async () => {
         await InlineDomain.humanizeText();
       },
     },
     {
-      label: "Fix spelling",
-      accelerator: shortcuts.spellCheck.join("+"),
-      role: "help",
+      label: 'Fix spelling',
+      accelerator: shortcuts.spellCheck.join('+'),
+      role: 'help',
       click: async () => {
         await InlineDomain.spellCheck();
       },
     },
     {
-      label: "Calculate",
-      accelerator: shortcuts.calculate.join("+"),
-      role: "help",
+      label: 'Calculate',
+      accelerator: shortcuts.calculate.join('+'),
+      role: 'help',
       click: async () => {
         await InlineDomain.calculate();
       },
     },
     {
-      label: "Convert currencies",
-      accelerator: shortcuts.convertCurrency.join("+"),
-      role: "help",
+      label: 'Convert currencies',
+      accelerator: shortcuts.convertCurrency.join('+'),
+      role: 'help',
       click: async () => {
         await InlineDomain.convertCurrency();
       },
     },
     {
-      label: "Shorten URL",
-      accelerator: shortcuts.shortenUrl.join("+"),
-      role: "help",
+      label: 'Shorten URL',
+      accelerator: shortcuts.shortenUrl.join('+'),
+      role: 'help',
       click: async () => {
         await InlineDomain.shortenUrl();
       },
     },
-    { label: "Separator", type: "separator" },
-    { label: "Quit", role: "quit", click: () => app.quit() },
+    { label: 'Separator', type: 'separator' },
+    { label: 'Quit', role: 'quit', click: () => app.quit() },
   ]);
   tray.setContextMenu(contextMenu);
 
-  tray.setToolTip("Nodetools");
+  tray.setToolTip('Nodetools');
 });
 
 export const relaunchApp = () => {
@@ -206,8 +231,8 @@ export const relaunchApp = () => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });

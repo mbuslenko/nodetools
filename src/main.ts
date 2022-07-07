ff10011bc3e2ee85ca2e1cf3cd928e98cdd0677b
Mike <49212643+mbuslenko@users.noreply.github.com>
2022-07-08 01:40:12 +0300

    Windows fixes

--- src/main.ts
+++ src/main.ts
@@ -282,12 +282,3 @@ app.whenReady().then(() => {
 
   tray.setToolTip('Nodetools');
 });
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
